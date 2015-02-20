7e3d9e00e61500a0535070fc5dfb2f3f83cb4be0
webvictim <webvictim@gmail.com>
2015-02-19 19:49:22 -0800

    Reminders now use pickle for storage and lowercase dictionary keys as IRC nicks are case-insensitive

--- reminder.py
+++ reminder.py
@@ -1,64 +1,55 @@
 """
 reminder.py - Willie Reminder Module
 Modifications by webvictim <webvictim@gmail.com>
-Further Modifications by jcrza
 """
 
 import os
 import re
+import pickle
 import time
-import threading
-import codecs
 from datetime import datetime
 from willie import module, web
 
+# list of ignored nicks
+ignored_nicks = ['mcbot']
+
 def filename(self):
-    name = self.nick + '-' + self.config.user + '.reminders.db'
+    name = self.nick + '-' + self.config.user + '.reminders-pickle.db'
     return os.path.join(self.config.dotdir, name)
 
 def load_database(name):
-    data = {}
-    if os.path.isfile(name):
-        f = codecs.open(name, 'r', encoding='utf-8')
-        for line in f:
-            db_key, unixtime, channel, nick, message = line.split('\t')
-            message = message.rstrip('\n')
-            reminder = (unixtime, channel, nick, message)
-            try:
-                data[db_key].append(reminder)
-            except KeyError:
-                data[db_key] = [reminder]
-        f.close()
-    return data
+    try:
+        return pickle.load(open(name, "rb"))
+    except IOError:
+        return {}
 
 def dump_database(name, data):
-    f = codecs.open(name, 'w', encoding='utf-8')
-    try:
-        for db_key, reminders in data.iteritems():
-            for unixtime, channel, nick, message in reminders:
-                f.write('%s\t%s\t%s\t%s\t%s\n' % (nick, unixtime, channel, nick, message))
-        f.close()
-    except RuntimeError:
-        pass
+    pickle.dump(data, open(name, "wb"))
 
 def setup(bot):
-    bot.rfn = filename(bot)
-    bot.rdb = load_database(bot.rfn)
+    bot.fn = filename(bot)
+    bot.rdb = load_database(bot.fn)
 
-@module.rule('^(?!\!remind$).*')
+# this is pretty inefficient as it runs through the entire list once for
+# every message that gets sent to any channel that the bot is part of
+# better way would be to make the reminders keyed on nick and just check that
+@module.rule('^(?!@remind|!remind).*')
+#@module.rule('.*')
 def reminder_check(bot, trigger):
+    check_nick = trigger.nick.lower()
+    if (trigger.nick in ignored_nicks):
+        return
     """Runs on every incoming message to see whether we have any reminders to give to the given nickname."""
-    db_keys = [key for key in bot.rdb]
-    if db_keys:
-        if trigger.nick in sorted(db_keys):
-            for (unixtime, channel, nick, message) in bot.rdb[trigger.nick]:
-                # rewrite the sender if it's yourself
-                if nick == trigger.nick:
-                    nick = 'you'
-                # send the reminder message to whatever channel the person spoke in
-                bot.msg(trigger.sender, "%s, %s asked me to remind you: %s" % (trigger.nick, nick, message))
-            del bot.rdb[trigger.nick]
-            dump_database(bot.rfn, bot.rdb)
+    had_reminder = False
+    if bot.rdb.has_key(check_nick):
+        for reminder in bot.rdb[check_nick]:
+            had_reminder = True
+            if reminder['from'].lower() == check_nick:
+                reminder['from'] = 'you'
+            bot.msg(trigger.sender, "%s, %s asked me to remind you: %s" % (trigger.nick, reminder['from'], reminder['message']))
+    if had_reminder:
+        discard = bot.rdb.pop(trigger.nick, None)
+    dump_database(bot.fn, bot.rdb)
 
 @module.rule("!remind$")
 @module.rule("!remind ([\S,]+)\ (.*)")
@@ -75,7 +66,7 @@ def remind(bot, trigger):
         return bot.reply("Syntax: !remind <nick>[,<nick>...] <message>")
 
     if target_nick == "me":
-        target_nick = trigger.nick; 
+        target_nick = trigger.nick 
 
     if "," in target_nick:
         # skip blank values (this usually happens when called like 
@@ -90,15 +81,15 @@ def create_reminder(bot, trigger, target_nick_list, message):
     reminder_counter = 0
     # built-in deduplication
     for nick_to_remind in list(set(target_nick_list)):
-        unixtime = int(time.time())
-        reminder = (unixtime, trigger.sender, trigger.nick, message)
+        lowercase_nick = nick_to_remind.lower()
+        data = {'time': int(time.time() * 1000), 'channel': trigger.sender, 'from': trigger.nick, 'to': nick_to_remind, 'message': message }
         try:
-            bot.rdb[nick_to_remind].append(reminder)
+            bot.rdb[lowercase_nick].append(data)
         except KeyError:
-            bot.rdb[nick_to_remind] = [reminder]
+            bot.rdb[lowercase_nick] = [data]
 
         reminder_counter += 1
-        dump_database(bot.rfn, bot.rdb)
+    dump_database(bot.fn, bot.rdb)
 
     if reminder_counter > 1:
         bot.reply("%s reminders remembered." % reminder_counter)
